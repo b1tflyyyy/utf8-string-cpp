bc57183ade0481f565be34480a0f9c57762ad226
b1tflyyyy <thisisandrew0110@gmail.com>
2024-03-19 14:07:01 -0400

    added additional tests for utf8::ustring

--- tests/utf8_string_test.cpp
+++ tests/utf8_string_test.cpp
@@ -62,3 +62,85 @@ TEST(utf8_string, replace_char_case_3)
 
     ASSERT_EQ(ustr.get_str(), "прمرحباпgт");
 }
+
+// ---------------------------------------------------------------------------------------------------------------------
+TEST(utf8_string, ctors)
+{
+    // ----------------------------- const char* PART -----------------------------
+    const char* c_str{ "привет" };
+
+    utf8::ustring ustr_from_c_str{ c_str };
+    ASSERT_EQ(c_str, ustr_from_c_str.get_c_str());
+
+    ustr_from_c_str.replace_char("g", 0);
+    ASSERT_NE(c_str, ustr_from_c_str.get_c_str());
+
+
+    // ----------------------------- std::string PART -----------------------------
+    std::string cpp_str{ "пока" };
+
+    utf8::ustring ustr_from_cpp_str{ cpp_str };
+    ASSERT_EQ(cpp_str, ustr_from_cpp_str.get_c_str());
+
+    ustr_from_cpp_str.replace_char("h", 0);
+    ASSERT_NE(cpp_str, ustr_from_cpp_str.get_c_str());
+
+
+    // ----------------------------- std::string_view PART -----------------------------
+    std::string_view cpp_str_view{ "что" };
+
+    utf8::ustring ustr_from_cpp_str_view{ cpp_str_view };
+    ASSERT_EQ(cpp_str_view, ustr_from_cpp_str_view.get_c_str());
+
+    ustr_from_cpp_str_view.replace_char("L", 0);
+    ASSERT_NE(cpp_str_view, ustr_from_cpp_str_view.get_c_str());
+
+
+    // ----------------------------- move ctor PART -----------------------------
+    const char* move_data{ "привет мир" };
+
+    utf8::ustring ustr_move{ move_data };
+    auto other_ustr_move{ std::move(ustr_move) };
+
+    ASSERT_EQ(other_ustr_move.get_c_str(), move_data);
+    ASSERT_NE(ustr_move.get_c_str(), move_data);
+
+
+    // ----------------------------- copy ctor PART -----------------------------
+    const char* copy_data{ "хеллоу ворлд" };
+
+    utf8::ustring copy_data_first{ copy_data };
+    utf8::ustring copy_data_second{ copy_data_first };
+
+    ASSERT_EQ(copy_data, copy_data_first.get_c_str());
+    ASSERT_EQ(copy_data, copy_data_second.get_c_str());
+    ASSERT_EQ(copy_data_first, copy_data_second);
+
+    // TODO: write for other ctors ....
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+TEST(utf8_string, operators)
+{
+    // ----------------------------- move operator PART -----------------------------
+    const char* move_data{ "hello world" };
+
+    utf8::ustring ustr_move{ move_data };
+    utf8::ustring other_ustr_move;
+    other_ustr_move = std::move(ustr_move);
+
+    ASSERT_EQ(other_ustr_move.get_c_str(), move_data);
+    ASSERT_NE(ustr_move.get_c_str(), move_data);
+
+
+    // ----------------------------- copy operator PART -----------------------------
+    const char* copy_data{ "хеллоу ворлд" };
+
+    utf8::ustring copy_data_first{ copy_data };
+    utf8::ustring copy_data_second;
+    copy_data_second = copy_data_first;
+
+    ASSERT_EQ(copy_data, copy_data_first.get_c_str());
+    ASSERT_EQ(copy_data, copy_data_second.get_c_str());
+    ASSERT_EQ(copy_data_first, copy_data_second);
+}
